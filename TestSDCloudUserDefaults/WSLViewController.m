e72ec6fa57c22eb22a08d7cccd3327f343379825
Stephen Darlington <stephen@zx81.org.uk>
2014-09-06 14:25:42 +0100

    Tidy up the sample code

--- TestSDCloudUserDefaults/WSLViewController.m
+++ TestSDCloudUserDefaults/WSLViewController.m
@@ -9,23 +9,7 @@
 #import "WSLViewController.h"
 #import "SDCloudUserDefaults.h"
 
-@interface WSLViewController ()
-
-@end
-
 @implementation WSLViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark - UITextFieldDelegate
 
